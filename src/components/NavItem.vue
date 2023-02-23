bf26adec7f7a475b194377443cae01fb6f0cfcbb
Paul Hathaway <paulchathaway@gmail.com>
2023-02-22 23:06:45 -0700

    create navitem component

---
+++ src/components/NavItem.vue
@@ -0,0 +1,22 @@
+<script lang="ts" setup>
+const props = defineProps({
+  iconType: {type: String, required: true}
+})
+</script>
+
+<template>
+  <div class="space"></div>
+  <font-awesome-icon class="icon" :icon="['fa-solid', props.iconType]"/>
+  <span><slot></slot> </span>
+</template>
+
+<style lang="less" scoped>
+.icon {
+  font-size: 1.5rem;
+  width: 2rem;
+}
+
+span {
+  margin-left: 0.5rem;
+}
+</style>
