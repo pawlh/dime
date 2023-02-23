1fd7495ba498912301a184859ebcfc40bc9ef616
Paul Hathaway <paulchathaway@gmail.com>
2023-02-22 23:05:52 -0700

    load fortawesome icons

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,17 @@
-import { createApp } from 'vue'
+import {createApp} from 'vue'
 import App from './App.vue'
 import router from './router'
+import {library} from '@fortawesome/fontawesome-svg-core'
+import {FontAwesomeIcon} from '@fortawesome/vue-fontawesome'
+import {faPiggyBank, faCalculator, faChartLine} from '@fortawesome/free-solid-svg-icons'
 
-import './assets/main.css'
+import './assets/main.less'
+
+library.add(faPiggyBank, faCalculator, faChartLine)
 
 const app = createApp(App)
 
 app.use(router)
 
-app.mount('#app')
+app.component('font-awesome-icon', FontAwesomeIcon)
+    .mount('#app')
