e716c78fe6013f5a4b84c64ccaca190fab2ebb91
Paul Hathaway <paulchathaway@gmail.com>
2023-02-24 19:21:39 -0700

    component-ize nav items

--- src/components/NavItem.vue
+++ src/components/NavItem.vue
@@ -1,16 +1,52 @@
 <script lang="ts" setup>
-const props = defineProps({
-  iconType: {type: String, required: true}
+import {useRouter} from "vue-router";
+
+defineProps({
+  iconType: {type: String, required: true},
+  destination: {type: String, required: true},
+  selected: {type: Boolean, default: false}
 })
+
+const router = useRouter();
+
+
 </script>
 
 <template>
-  <div class="space"></div>
-  <font-awesome-icon class="icon" :icon="['fa-solid', props.iconType]"/>
-  <span><slot></slot> </span>
+  <div class="container"
+       :class="{selected}"
+       @click="() => {router.push({name: destination})}">
+    <div class="space">&nbsp;</div>
+    <font-awesome-icon class="icon" :icon="['fa-solid', iconType]"/>
+    <span><slot></slot></span>
+  </div>
 </template>
 
 <style lang="less" scoped>
+.container {
+  display: flex;
+  flex-direction: row;
+
+  cursor: pointer;
+
+  &:hover {
+    color: var(--color-accent);
+  }
+}
+
+.space {
+  width: .35rem;
+  margin-right: 1rem;
+}
+
+.selected {
+  color: var(--color-accent);
+
+  > .space {
+    background-color: var(--color-accent);
+  }
+}
+
 .icon {
   font-size: 1.5rem;
   width: 2rem;
