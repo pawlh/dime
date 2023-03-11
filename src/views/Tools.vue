cad61f3c9bc2953bdb62f2c63af22e2cce9567cd
Paul Hathaway <paulchathaway@gmail.com>
2023-03-10 19:16:19 -0700

    add horizontal bar navigation

--- src/views/Tools.vue
+++ src/views/Tools.vue
@@ -1,21 +1,88 @@
 <script setup>
+import {useRoute} from "vue-router";
+
+const route = useRoute();
 </script>
 
 <template>
   <div class="tools">
     <div class="options">
-      <router-link to="/tools/import">Import Transactions</router-link>
-      <router-link to="/tools/accounts">Manage Accounts</router-link>
+      <div class="nav-option" :class="{selected: route.name === 'import'}">
+        <router-link :to="{name: 'import'}">Import Transactions</router-link>
+      </div>
+      <div class="nav-option" :class="{selected: route.name === 'accounts'}">
+        <router-link :to="{name: 'accounts'}">Manage Accounts</router-link>
+      </div>
     </div>
-    <router-view></router-view>
+    <router-view class="content"></router-view>
   </div>
 </template>
 
 <style lang="less" scoped>
+@import "@/assets/mixins.less";
+
+.tools {
+  background-color: var(--color-background-soft);
+  box-shadow: 0 0 10px rgba(0, 0, 0, 0.3);
+}
+
 .options {
+  background-color: var(--color-background-lightest);
+
+  //align-items: center;
+
+
+  @media @md-desktop {
+    width: 100%;
+    min-height: 50px;
+
+    display: flex;
+
+    flex-direction: row;
+    justify-content: space-evenly;
+    column-gap: 5px;
+
+  }
+}
+
+.nav-option {
+  width: 100%;
+  //height: 100%;
+  text-align: center;
+
+
   display: flex;
-  flex-direction: column;
   align-items: center;
+
+  & > a {
+    color: var(--color-background-soft);
+    flex-grow: 1;
+
+    font-weight: bold;
+
+    &:hover {
+      background-color: unset;
+    }
+  }
+
+  border-bottom: 5px solid transparent;
+
+  transition: border-bottom-color 0.2s ease-in;
+}
+
+//.nav-option > a {
+//  color: var(--color-background-soft);
+//}
+
+.selected {
+  border-bottom: 5px solid var(--color-accent);
+}
+
+.content {
+}
+
+a:link, a:visited, a:hover, a:active {
+  text-decoration: none;
 }
 
 </style>
