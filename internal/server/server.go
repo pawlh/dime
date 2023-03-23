ce8e94ae0132654a9f9032917693da15b2ff81f4
Paul Hathaway <paulchathaway@gmail.com>
2023-03-22 18:11:21 -0600

    init server

---
+++ internal/server/server.go
@@ -0,0 +1,14 @@
+package server
+
+import (
+	"github.com/labstack/echo/v4"
+	"net/http"
+)
+
+func start() {
+	e := echo.New()
+	e.GET("/", func(c echo.Context) error {
+		return c.String(http.StatusOK, "Hello, World!")
+	})
+	e.Logger.Fatal(e.Start(":1323"))
+}
