c4fc1daf33efc8bd2bab121dc72eafea3cd76398
Paul Hathaway <paulchathaway@gmail.com>
2023-03-22 19:10:52 -0600

    register routes in separate method

--- internal/server/server.go
+++ internal/server/server.go
@@ -1,14 +1,15 @@
 package server
 
 import (
+	"dime/internal/api"
 	"github.com/labstack/echo/v4"
-	"net/http"
 )
 
-func start() {
+func Start() {
+
 	e := echo.New()
-	e.GET("/", func(c echo.Context) error {
-		return c.String(http.StatusOK, "Hello, World!")
-	})
+
+	api.RegisterRoutes(e)
+
 	e.Logger.Fatal(e.Start(":1323"))
 }
