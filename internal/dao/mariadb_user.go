9026dc3955299132398ac0e84bf1d5746a8948a8
Paul Hathaway <paulchathaway@gmail.com>
2023-03-23 01:56:29 -0600

    add name handling to daos

--- internal/dao/mariadb_user.go
+++ internal/dao/mariadb_user.go
@@ -13,19 +13,22 @@ func NewMariaDbUser(db *sql.DB) MariaDbUser {
 	return MariaDbUser{db: db}
 }
 
-func (m MariaDbUser) Insert(user *models.User) error {
-	_, err := m.db.Exec("INSERT INTO user (username, password) VALUES (?, ?)",
+func (m MariaDbUser) Create(user *models.User) error {
+	_, err := m.db.Exec("INSERT INTO user (username, password, name) VALUES (?, ?, ?)",
 		user.Username,
 		user.Password,
+		user.Name,
 	)
 	return err
 }
 
 func (m MariaDbUser) FindByUsername(username string) (*models.User, error) {
-	row := m.db.QueryRow("SELECT username, password FROM user WHERE username = ?", username)
+	row := m.db.QueryRow("SELECT username, password, name FROM user WHERE username = ?", username)
 	user := new(models.User)
 	err := row.Scan(&user.Username, &user.Password)
-	if err != nil {
+	if err == sql.ErrNoRows {
+		return nil, UserNotFound{}
+	} else if err != nil {
 		return nil, err
 	}
 	return user, nil
--- internal/dao/user_dao.go
+++ internal/dao/user_dao.go
@@ -2,7 +2,13 @@ package dao
 
 import "dime/internal/models"
 
+type UserNotFound struct{}
+
+func (e UserNotFound) Error() string {
+	return "user not found"
+}
+
 type UserDAO interface {
-	Insert(user *models.User) error
+	Create(user *models.User) error
 	FindByUsername(username string) (*models.User, error)
 }
