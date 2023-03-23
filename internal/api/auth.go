a7475b4d52841c947c9e09fb764e9c5724f2dc77
Paul Hathaway <paulchathaway@gmail.com>
2023-03-23 01:56:16 -0600

    add register endpoint

--- internal/api/auth.go
+++ internal/api/auth.go
@@ -1,8 +1,10 @@
 package api
 
 import (
+	"dime/internal/dao"
 	"dime/internal/dbs"
 	"dime/internal/models"
+	"errors"
 	"github.com/golang-jwt/jwt/v4"
 	"github.com/labstack/echo/v4"
 	"net/http"
@@ -31,6 +33,62 @@ func Login(c echo.Context) error {
 		return mustSendError(c, http.StatusUnauthorized, "bad credentials")
 	}
 
+	token, err := generateToken(user)
+	if err != nil {
+		return mustSendError(c, http.StatusInternalServerError, "error generating token")
+	}
+
+	if err != nil {
+		return mustSendError(c, http.StatusInternalServerError, "error signing token")
+	}
+
+	if err = c.JSON(http.StatusOK, echo.Map{
+		"token": token,
+	}); err != nil {
+		return mustSendError(c, http.StatusInternalServerError, "error sending token")
+	}
+
+	return nil
+}
+
+func Register(c echo.Context) error {
+	var user models.User
+
+	if err := c.Bind(&user); err != nil {
+		return mustSendError(c, http.StatusBadRequest, "missing username and/or password")
+	}
+
+	if user.Name == "" || user.Username == "" || user.Password == "" {
+		return mustSendError(c, http.StatusBadRequest, "missing required fields")
+	}
+
+	if match, err := dbs.DB.UserDao().FindByUsername(user.Username); err != nil {
+		if !errors.As(err, &dao.UserNotFound{}) {
+			return mustSendError(c, http.StatusInternalServerError, "error searching existing users")
+		}
+	} else if match.Username != "" {
+		return mustSendError(c, http.StatusConflict, "username already exists")
+	}
+
+	if err := dbs.DB.UserDao().Create(&user); err != nil {
+		return mustSendError(c, http.StatusInternalServerError, "error creating user")
+	}
+
+	token, err := generateToken(user)
+	if err != nil {
+		return mustSendError(c, http.StatusInternalServerError, "error generating token")
+	}
+
+	if err := c.JSON(http.StatusOK, echo.Map{
+		"token": token,
+	}); err != nil {
+		return mustSendError(c, http.StatusInternalServerError, "error sending message")
+	}
+
+	return nil
+}
+
+func generateToken(user models.User) (string, error) {
 	claims := &jwtCustomClaims{
 		user.Username,
 		user.Name,
@@ -41,17 +99,8 @@ func Login(c echo.Context) error {
 	}
 	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
 	signedToken, err := token.SignedString(secret)
-	if err != nil {
-		return mustSendError(c, http.StatusInternalServerError, "error signing token")
-	}
 
-	if err = c.JSON(http.StatusOK, echo.Map{
-		"token": signedToken,
-	}); err != nil {
-		return mustSendError(c, http.StatusInternalServerError, "error sending token")
-	}
-
-	return nil
+	return signedToken, err
 }
 
 func validateToken(next echo.HandlerFunc) echo.HandlerFunc {
