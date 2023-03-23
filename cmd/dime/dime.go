78f620a944436bd2d717b1b208a8cfe3f3059878
Paul Hathaway <paulchathaway@gmail.com>
2023-03-22 19:11:00 -0600

    remove tests from main

--- cmd/dime/dime.go
+++ cmd/dime/dime.go
@@ -1,38 +1,17 @@
 package main
 
 import (
-	"database/sql"
-	"dime/internal/dao"
 	"dime/internal/dbs"
-	"dime/internal/models"
+	"dime/internal/server"
 	"log"
-	"time"
 )
 
 func main() {
-	db, err := dbs.InitMariaDB()
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer func(db *sql.DB) {
-		err := db.Close()
-		if err != nil {
-			log.Fatal(err)
-		}
-	}(db)
 
-	sampleTransaction := models.Transaction{
-		Date:        time.Now(),
-		Description: "Sample transaction",
-		Amount:      100.00,
-		Category:    "Sample category",
-		Account:     "Sample account",
-	}
-
-	transactionDao := dao.NewMariaDbTransaction(db)
-	err = transactionDao.Insert(&sampleTransaction)
+	err := dbs.InitMariaDB()
 	if err != nil {
 		log.Fatal(err)
 	}
 
+	server.Start()
 }
