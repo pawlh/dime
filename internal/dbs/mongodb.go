2c33e584b13d005313f333c3980a83114cc794f2
Paul Hathaway <paulchathaway@gmail.com>
2023-03-24 16:21:47 -0600

    create database init for mongodb

---
+++ internal/dbs/mongodb.go
@@ -0,0 +1,51 @@
+package dbs
+
+import (
+	"context"
+	"dime/internal/dao"
+	"dime/internal/dao/mongodb"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+	"time"
+)
+
+type MongoDB struct {
+	client *mongo.Client
+}
+
+func (m MongoDB) UserDao() dao.UserDAO {
+	return mongodb.NewUser(m.client)
+}
+
+func (m MongoDB) TransactionDao() dao.TransactionDao {
+	return nil
+}
+
+func (m MongoDB) ArchiveDao() dao.ArchiveDao {
+	return nil
+}
+
+func InitMongoDB() error {
+	client, err := mongo.NewClient(options.Client().ApplyURI("mongodb://localhost:27017"))
+	if err != nil {
+		return err
+	}
+
+	ctx, _ := context.WithTimeout(context.Background(), 10*time.Second)
+
+	err = client.Connect(ctx)
+	if err != nil {
+		return err
+	}
+
+	err = client.Connect(nil)
+	if err != nil {
+		return err
+	}
+
+	DB = MongoDB{
+		client: client,
+	}
+
+	return nil
+}
