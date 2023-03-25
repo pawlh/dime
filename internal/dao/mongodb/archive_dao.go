515dcea9ead5450387aeb5a172d3fa9d41e4592b
Paul Hathaway <paulchathaway@gmail.com>
2023-03-24 20:05:10 -0600

    remove ID before inserting

--- internal/dao/mongodb/archive_dao.go
+++ internal/dao/mongodb/archive_dao.go
@@ -14,6 +14,10 @@ func NewArchive(client *mongo.Client) Archive {
 }
 
 func (m Archive) Create(archive *models.Archive) error {
+
+	// the ID is set by the database
+	archive.ID = ""
+
 	collections := m.client.Database("dime").Collection("archive")
 	_, err := collections.InsertOne(nil, archive)
 	if err != nil {
