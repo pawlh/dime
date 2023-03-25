108a47cf04feec4bbc941fd3ae6e3d965a7af4a5
Paul Hathaway <paulchathaway@gmail.com>
2023-03-24 19:51:19 -0600

    pull out TestMain and helper functions

--- internal/dao/mongodb/user_dao_test.go
+++ internal/dao/mongodb/user_dao_test.go
@@ -1,51 +1,10 @@
 package mongodb
 
 import (
-	"context"
 	"dime/internal/models"
-	"go.mongodb.org/mongo-driver/mongo"
-	"go.mongodb.org/mongo-driver/mongo/options"
-	"log"
 	"reflect"
 	"testing"
 )
-
-var testURI = "mongodb://localhost:27018"
-
-func newClient() (*mongo.Client, error) {
-	client, err := mongo.NewClient(options.Client().ApplyURI(testURI))
-	if err != nil {
-		return nil, err
-	}
-	err = client.Connect(nil)
-	if err != nil {
-		return nil, err
-	}
-	return client, nil
-}
-
-var client *mongo.Client
-
-func TestMain(m *testing.M) {
-	var err error
-	client, err = newClient()
-	if err != nil {
-		panic(err)
-	}
-	defer func(client *mongo.Client, ctx context.Context) {
-		err := client.Disconnect(ctx)
-		if err != nil {
-			log.Fatal(err)
-		}
-	}(client, nil)
-
-	m.Run()
-
-	err = client.Database("dime").Drop(nil)
-	if err != nil {
-		log.Fatalf("Error dropping database: %v", err)
-	}
-}
 
 func TestUserDao_Create_Find(t *testing.T) {
 	testUser := models.User{
