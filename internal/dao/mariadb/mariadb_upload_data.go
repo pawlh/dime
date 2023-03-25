d5fcc6634a81dab083077b5aa74ab651fa609313
Paul Hathaway <paulchathaway@gmail.com>
2023-03-24 19:38:30 -0600

    temporarily remove broken mariadb code

--- internal/dao/mariadb/mariadb_upload_data.go
+++ internal/dao/mariadb/mariadb_upload_data.go
@@ -3,7 +3,6 @@ package mariadb
 import (
 	"database/sql"
 	"dime/internal/models"
-	"github.com/labstack/gommon/log"
 )
 
 type UploadData struct {
@@ -15,39 +14,40 @@ func NewMariaDbArchive(db *sql.DB) UploadData {
 }
 
 func (m UploadData) Create(uploadData *models.Archive) error {
-	_, err := m.db.Exec("INSERT INTO archive (upload_date, file_name, original_name, owner) VALUES (?, ?, ?, ?)",
-		uploadData.UploadDate,
-		uploadData.FileName,
-		uploadData.OriginalName,
-		uploadData.Owner,
-	)
-	if err != nil {
-		return err
-	}
-
+	//_, err := m.db.Exec("INSERT INTO archive (upload_date, file_name, original_name, owner) VALUES (?, ?, ?, ?)",
+	//	uploadData.UploadDate,
+	//	uploadData.FileName,
+	//	uploadData.OriginalName,
+	//	uploadData.Owner,
+	//)
+	//if err != nil {
+	//	return err
+	//}
+	//
 	return nil
 }
 
 func (m UploadData) FindByOwner(owner string) ([]models.Archive, error) {
-	rows, err := m.db.Query("SELECT * FROM archive WHERE owner = ?", owner)
-	if err != nil {
-		return nil, err
-	}
-	defer func(rows *sql.Rows) {
-		err := rows.Close()
-		if err != nil {
-			log.Error(err)
-		}
-	}(rows)
-
-	var uploadData []models.Archive
-	for rows.Next() {
-		var ud models.Archive
-		err := rows.Scan(&ud.ID, &ud.UploadDate, &ud.FileName, &ud.OriginalName, &ud.Owner)
-		if err != nil {
-			return nil, err
-		}
-		uploadData = append(uploadData, ud)
-	}
-	return uploadData, nil
+	//rows, err := m.db.Query("SELECT * FROM archive WHERE owner = ?", owner)
+	//if err != nil {
+	//	return nil, err
+	//}
+	//defer func(rows *sql.Rows) {
+	//	err := rows.Close()
+	//	if err != nil {
+	//		log.Error(err)
+	//	}
+	//}(rows)
+	//
+	//var uploadData []models.Archive
+	//for rows.Next() {
+	//	var ud models.Archive
+	//	err := rows.Scan(&ud.ID, &ud.UploadDate, &ud.FileName, &ud.OriginalName, &ud.Owner)
+	//	if err != nil {
+	//		return nil, err
+	//	}
+	//	uploadData = append(uploadData, ud)
+	//}
+	//return uploadData, nil
+	return nil, nil
 }
