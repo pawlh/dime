c12631ca69e0eef6f9ac64f2d83f7ef31a93e6eb
Paul Hathaway <paulchathaway@gmail.com>
2023-03-30 16:59:20 -0600

    cleanup unused imports

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,12 +1,5 @@
 <script setup lang="ts">
 import {RouterView} from 'vue-router'
-import Navigation from "@/views/Navigation.vue";
-import ProfilePreview from "@/components/ProfilePreview.vue";
-import FloatingActionButton from "@/components/FloatingActionButton.vue";
-
-import {useRouter} from "vue-router";
-import {useStateStore} from "@/store/state";
-import Login from "@/views/Login.vue";
 
 // Default application to dark mode
 document.documentElement.setAttribute("theme", "dark");
