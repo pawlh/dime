5fa89e995125d0e4d4ea4c1b4074c42fc9a89e36
Paul Hathaway <paulchathaway@gmail.com>
2023-03-29 18:04:44 -0600

    add tests for /login and /register

--- internal/api/auth_test.go
+++ internal/api/auth_test.go
@@ -3,6 +3,8 @@ package api
 import (
 	"dime/internal/models"
 	"github.com/golang-jwt/jwt/v4"
+	"github.com/labstack/echo/v4"
+	"net/http"
 	"testing"
 )
 
@@ -31,5 +33,119 @@ func TestGenerateToken(t *testing.T) {
 	}
 	if customClaim.Name != user.Name {
 		t.Fatal(`token.Name != user.Name, got: ` + customClaim.Name + `, want: ` + user.Name)
+	}
+}
+
+func TestLogin(t *testing.T) {
+	BeforeEach()
+	InsertTestUser()
+
+	tests := []struct {
+		name         string
+		body         *models.User
+		expectedCode int
+	}{
+		{
+			name: "Missing Username",
+			body: &models.User{
+				Password: "password",
+				Name:     "name",
+			},
+			expectedCode: http.StatusBadRequest,
+		},
+		{
+			name: "Missing Password",
+			body: &models.User{
+				Username: "username",
+				Name:     "name",
+			},
+			expectedCode: http.StatusBadRequest,
+		},
+		{
+			"Valid Login",
+			testUser,
+			http.StatusOK,
+		},
+		{
+			"Password is incorrect",
+			&models.User{
+				Username: testUser.Username,
+				Password: "fake",
+			},
+			http.StatusUnauthorized,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			res, resBody := ServeAndRequest(echo.GET, "/api/login", Login, tt.body)
+
+			if res.Code != tt.expectedCode {
+				t.Fatalf("expected status code %d, got %d with message \"%s\"", tt.expectedCode, res.Code, resBody["error"])
+			}
+
+			if tt.expectedCode == http.StatusOK && resBody["token"] == "" {
+				t.Fatal("expected token, got empty string")
+			}
+
+		})
+	}
+}
+
+func TestRegister(t *testing.T) {
+	BeforeEach()
+
+	tests := []struct {
+		name         string
+		body         *models.User
+		expectedCode int
+	}{
+		{
+			name: "Missing Username",
+			body: &models.User{
+				Password: "password",
+				Name:     "name",
+			},
+			expectedCode: http.StatusBadRequest,
+		},
+		{
+			name: "Missing Password",
+			body: &models.User{
+				Username: "username",
+				Name:     "name",
+			},
+			expectedCode: http.StatusBadRequest,
+		},
+		{
+			name: "Missing Name",
+			body: &models.User{
+				Username: "username",
+				Password: "password",
+			},
+			expectedCode: http.StatusBadRequest,
+		},
+		{
+			"Valid Register",
+			testUser,
+			http.StatusOK,
+		},
+		{
+			"User Already Exists",
+			testUser,
+			http.StatusConflict,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			res, resBody := ServeAndRequest(echo.GET, "/api/register", Register, tt.body)
+
+			if res.Code != tt.expectedCode {
+				t.Fatalf("expected status code %d, got %d with message \"%s\"", tt.expectedCode, res.Code, resBody["error"])
+			}
+
+			if tt.expectedCode == http.StatusOK && resBody["token"] == "" {
+				t.Fatal("expected token, got empty string")
+			}
+
+		})
 	}
 }
