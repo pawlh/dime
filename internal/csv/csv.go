4829967f0b920245e641c93334239b16a64d17ce
Paul Hathaway <paulchathaway@gmail.com>
2023-04-02 17:21:30 -0600

    add and test method to adapt required columns

--- internal/csv/csv.go
+++ internal/csv/csv.go
@@ -2,10 +2,14 @@ package csv
 
 import (
 	"bytes"
+	"dime/internal/models"
 	"encoding/csv"
+	"fmt"
+	"strconv"
+	"time"
 )
 
-func Parse(buf *bytes.Buffer) ([]map[string]string, error) {
+func Parse(buf *bytes.Buffer) ([]map[string]any, error) {
 
 	reader := csv.NewReader(buf)
 
@@ -14,7 +18,7 @@ func Parse(buf *bytes.Buffer) ([]map[string]string, error) {
 		return nil, err
 	}
 
-	var data []map[string]string
+	var data []map[string]any
 
 	for {
 		row, err := reader.Read()
@@ -22,7 +26,7 @@ func Parse(buf *bytes.Buffer) ([]map[string]string, error) {
 			break
 		}
 
-		rowData := make(map[string]string)
+		rowData := make(map[string]any)
 
 		for i, field := range header {
 			rowData[field] = row[i]
@@ -34,13 +38,49 @@ func Parse(buf *bytes.Buffer) ([]map[string]string, error) {
 	return data, nil
 }
 
-func RenameColumns(data []map[string]string, columnMapping map[string]string) []map[string]string {
+func RenameColumns(data []map[string]any, columnMapping map[string]string) []map[string]any {
+	var renamedData []map[string]any
 	for i, row := range data {
+		renamedData = append(renamedData, make(map[string]any))
 		for oldName, newName := range columnMapping {
-			data[i][newName] = row[oldName]
-			delete(data[i], oldName)
+			renamedData[i][newName] = row[oldName]
 		}
 	}
 
-	return data
+	return renamedData
+}
+
+func AdaptRequiredFields(data []map[string]any, dateFormat string) error {
+	for _, row := range data {
+		for columnName, columnType := range models.RequiredColumns {
+			if _, ok := row[columnName]; !ok {
+				return fmt.Errorf("column %s is missing", columnName)
+			}
+
+			switch columnType {
+			case "string":
+				//this should never be false
+				if _, ok := row[columnName].(string); !ok {
+					return fmt.Errorf("column %s is not a string", columnName)
+				}
+			case "date":
+				date, err := time.Parse(dateFormat, row[columnName].(string))
+				if err != nil {
+					return fmt.Errorf("could not format %s", row[columnName])
+				}
+				row[columnName] = date
+			case "float":
+				// convert string to float
+				number, err := strconv.ParseFloat(row[columnName].(string), 64)
+				if err != nil {
+					return fmt.Errorf("could not format %s", row[columnName])
+				}
+				row[columnName] = number
+			default:
+				return fmt.Errorf("unknown column type %s", columnType)
+			}
+		}
+	}
+
+	return nil
 }
--- internal/csv/csv_test.go
+++ internal/csv/csv_test.go
@@ -3,16 +3,17 @@ package csv
 import (
 	"reflect"
 	"testing"
+	"time"
 )
 
 func TestRenameColumns(t *testing.T) {
-	original := []map[string]string{
+	original := []map[string]any{
 		{
-			"dAtE": "2020-01-01",
+			"dAtE": time.Now(),
 			"old":  "value",
 		},
 		{
-			"dAtE": "2020-01-02",
+			"dAtE": time.Now().Add(time.Hour * 24),
 			"old":  "value",
 		},
 	}
@@ -22,13 +23,13 @@ func TestRenameColumns(t *testing.T) {
 		"old":  "new",
 	}
 
-	expected := []map[string]string{
+	expected := []map[string]any{
 		{
-			"date": "2020-01-01",
+			"date": original[0]["dAtE"],
 			"new":  "value",
 		},
 		{
-			"date": "2020-01-02",
+			"date": original[1]["dAtE"],
 			"new":  "value",
 		},
 	}
@@ -38,4 +39,124 @@ func TestRenameColumns(t *testing.T) {
 	if !reflect.DeepEqual(actual, expected) {
 		t.Errorf("RenameColumns() = %v, want %v", actual, expected)
 	}
+}
+
+func TestAdaptRequiredFields(t *testing.T) {
+	type args struct {
+		data       []map[string]any
+		dateFormat string
+	}
+	tests := []struct {
+		name     string
+		args     args
+		expected []map[string]any
+	}{
+		{
+			name: "All required fields are present",
+			args: args{
+				data: []map[string]any{
+					{
+						"Date":        "2020-01-01",
+						"Description": "test",
+						"Amount":      "1.00",
+						"Category":    "test",
+						"Account":     "test",
+					},
+				},
+				dateFormat: "2006-01-02",
+			},
+			expected: []map[string]any{
+				{
+					"Date":        mustParseTime("2020-01-01", "2006-01-02"),
+					"Description": "test",
+					"Amount":      1.00,
+					"Category":    "test",
+					"Account":     "test",
+				},
+			},
+		},
+		{
+			name: "Some fields are missing",
+			args: args{
+				data: []map[string]any{
+					{
+						"Date":     "2020-01-01",
+						"Amount":   "1.00",
+						"Category": "test",
+					},
+				},
+				dateFormat: "2006-01-02",
+			},
+			expected: nil,
+		},
+		{
+			name: "Date is malformed",
+			args: args{
+				data: []map[string]any{
+					{
+						"Date":        "202f0-c01-01ab",
+						"Description": "test",
+						"Amount":      "1.00",
+						"Category":    "test",
+						"Account":     "test",
+					},
+				},
+				dateFormat: "2006-01-02",
+			},
+			expected: nil,
+		},
+		{
+			name: "Amount is not a number",
+			args: args{
+				data: []map[string]any{
+					{
+						"Date":        "2020-01-01",
+						"Description": "test",
+						"Amount":      "abc",
+						"Category":    "test",
+						"Account":     "test",
+					},
+				},
+				dateFormat: "2006-01-02",
+			},
+			expected: nil,
+		},
+		{
+			name: "dateFormat is invalid",
+			args: args{
+				data: []map[string]any{
+					{
+						"Date":        "2020-01-01",
+						"Description": "test",
+						"Amount":      "1.00",
+						"Category":    "test",
+						"Account":     "test",
+					},
+				},
+				dateFormat: "a2006-0d1-02d",
+			},
+			expected: nil,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if err := AdaptRequiredFields(tt.args.data, tt.args.dateFormat); (err != nil) != (tt.expected == nil) {
+				t.Errorf("Error expected, but none was returned")
+			} else if tt.expected != nil {
+
+				if !reflect.DeepEqual(tt.args.data, tt.expected) {
+					t.Errorf("AdaptRequiredFields() = %v, want %v", tt.args.data, tt.expected)
+				}
+			}
+		})
+	}
+}
+
+func mustParseTime(s, layout string) time.Time {
+	t, err := time.Parse(layout, s)
+	if err != nil {
+		panic(err)
+	}
+	return t
 }
