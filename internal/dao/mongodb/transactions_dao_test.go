72567d884f0c7c98662095b4458514a7fdb79565
Paul Hathaway <paulchathaway@gmail.com>
2023-04-01 21:18:30 -0600

    add test for appending additional transactions

--- internal/dao/mongodb/transactions_dao_test.go
+++ internal/dao/mongodb/transactions_dao_test.go
@@ -45,6 +45,66 @@ func TestTransactions_InsertNew(t *testing.T) {
 	}
 }
 
+func TestTransactions_AppendToExisting(t *testing.T) {
+	BeforeEach()
+
+	transactionsDao := NewTransactions(client)
+
+	testTransaction := models.Transactions{
+		Transactions: []map[string]any{
+			{
+				"testColumn1": "transaction1",
+				"testColumn2": int32(100),
+			},
+		},
+		Owner:   "testUser",
+		Columns: []string{"testColumn1", "testColumn2"},
+	}
+
+	if err := transactionsDao.Insert(&testTransaction); err != nil {
+		t.Errorf("Error inserting new transactions: %v", err)
+	}
+
+	additionalTransactions := models.Transactions{
+		Transactions: []map[string]any{
+			{
+				"testColumn1": "transaction2",
+				"testColumn2": int32(200),
+			},
+		},
+		Owner:   "testUser",
+		Columns: []string{"testColumn1", "testColumn2"},
+	}
+
+	if err := transactionsDao.Insert(&additionalTransactions); err != nil {
+		t.Errorf("Error inserting new transactions: %v", err)
+	}
+
+	if match, err := transactionsDao.FindByOwner("testUser"); err != nil {
+		t.Errorf("Error finding transactions: %v", err)
+	} else {
+		expectedTransactions := models.Transactions{
+			Transactions: []map[string]any{
+				{
+					"testColumn1": "transaction1",
+					"testColumn2": int32(100),
+				},
+				{
+					"testColumn1": "transaction2",
+					"testColumn2": int32(200),
+				},
+			},
+			Owner:   "testUser",
+			Columns: []string{"testColumn1", "testColumn2"},
+		}
+
+		if !reflect.DeepEqual(*match, expectedTransactions) {
+			t.Errorf("Transactions do not match. Expected %v, got %v", expectedTransactions, *match)
+		}
+	}
+
+}
+
 func TestTransactions_FindByOwner(t *testing.T) {
 	BeforeEach()
 
