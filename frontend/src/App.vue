885c21a223fcf4f2b40dc8c4af974e68ae2ecac9
Paul Hathaway <paulchathaway@gmail.com>
2023-04-05 01:13:40 -0600

    add websocket utility file

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,8 +1,14 @@
 <script setup lang="ts">
 import {RouterView} from 'vue-router'
+import {setupTransactionWS} from "@/utils/transactions";
 
 // Default application to dark mode
 document.documentElement.setAttribute("theme", "dark");
+
+
+//connect to websocket at /api/transactions
+setupTransactionWS()
+
 
 </script>
 
---
+++ frontend/src/utils/transactions.js
@@ -0,0 +1,26 @@
+import {useStateStore} from "@/store/state";
+
+export const setupTransactionWS = () => {
+    const ws = new WebSocket("ws://localhost:1323/api/transactions");
+    ws.onopen = () => {
+        console.log("Connected to websocket");
+    };
+    ws.onmessage = (event) => {
+        console.log("Received message from websocket");
+        // console.log(event.data);
+
+        const data = JSON.parse(event.data);
+        if ('transactions' in data) {
+            const stateStore = useStateStore();
+
+            stateStore.transactions = data.transactions;
+        }
+    };
+    ws.onclose = () => {
+        console.log("Disconnected from websocket");
+    };
+    ws.onerror = (event) => {
+        console.log("Error with websocket");
+        console.log(event);
+    };
+}
