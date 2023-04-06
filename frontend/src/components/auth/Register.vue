7d0eb01c4099feff20f6bd805e60d827c7898617
Paul Hathaway <paulchathaway@gmail.com>
2023-04-05 21:15:49 -0600

    login after registering

--- frontend/src/components/auth/Register.vue
+++ frontend/src/components/auth/Register.vue
@@ -3,8 +3,10 @@ import {useRouter} from "vue-router";
 import {useStateStore} from "@/store/state";
 import {SERVER_URL} from "@/store/app";
 import {ref} from "vue";
+import {useCookies} from "vue3-cookies";
 
 const stateStore = useStateStore();
+const { cookies } = useCookies();
 
 const router = useRouter();
 
@@ -34,6 +36,12 @@ const register = async () => {
             error.value = ''
             stateStore.loggedInUser.name = data.name;
             stateStore.loggedIn = true;
+
+            // TEMPORARY: ultimately the token will be stored as an httpOnly cookie
+            cookies.set('token', data.token, {
+                expires: 4 * 60 * 60
+            })
+
             await router.push({name: 'home'});
         } else if (res.status === 409) {
             error.value = "Username already exists"
