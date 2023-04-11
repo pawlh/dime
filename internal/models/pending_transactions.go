14dc4bfc41129690eb1eaee9f0e858a50ac61974
Paul Hathaway <paulchathaway@gmail.com>
2023-04-10 23:23:18 -0600

    create model for new collection

---
+++ internal/models/pending_transactions.go
@@ -0,0 +1,9 @@
+package models
+
+// PendingTransactions holds transactions that have not yet been submitted by a user
+type PendingTransactions struct {
+	WIPTransactions   []map[string]any `bson:"wip_transactions" json:"wip_transactions"`
+	SavedTransactions []map[string]any `bson:"transactions" json:"transactions"`
+	Owner             string           `bson:"owner" json:"owner"`
+	Name              string           `bson:"name" json:"name"`
+}
