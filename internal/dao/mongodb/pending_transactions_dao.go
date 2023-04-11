6f37b13ea5500167c6e86735ef7c63f5f6508d2d
Paul Hathaway <paulchathaway@gmail.com>
2023-04-10 23:26:51 -0600

    create initial implementation

---
+++ internal/dao/mongodb/pending_transactions_dao.go
@@ -0,0 +1,39 @@
+package mongodb
+
+import (
+	"dime/internal/models"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo"
+)
+
+type PendingTransactions struct {
+	client *mongo.Client
+}
+
+func NewPendingTransactions(client *mongo.Client) PendingTransactions {
+	return PendingTransactions{client: client}
+}
+
+// Create saves a new pending transaction to the database
+func (m PendingTransactions) Create(transactions *models.PendingTransactions) error {
+	collection := m.client.Database("dime").Collection("transactions")
+
+	_, err := collection.InsertOne(nil, transactions)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (m PendingTransactions) FindByOwner(owner string) (*models.PendingTransactions, error) {
+	collection := m.client.Database("dime").Collection("transactions")
+
+	var transactions models.PendingTransactions
+	err := collection.FindOne(nil, bson.M{"owner": owner}).Decode(&transactions)
+	if err != nil {
+		return nil, err
+	}
+
+	return &transactions, nil
+}
