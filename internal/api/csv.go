d3154bcdae540870620fcf53fff6c23b579ee072
Paul Hathaway <paulchathaway@gmail.com>
2023-04-11 21:01:49 -0600

    write to pending table instead of permanent

--- internal/api/csv.go
+++ internal/api/csv.go
@@ -5,26 +5,18 @@ import (
 	"dime/internal/csv"
 	"dime/internal/dbs"
 	"dime/internal/models"
-	"encoding/json"
 	"fmt"
 	"github.com/labstack/echo/v4"
 	"io"
 	"mime/multipart"
 	"net/http"
+	"time"
 	_ "time"
 )
 
-//var dateLayout = "2006-01-02 15:04:05"
-
-type meta struct {
-	ColumnMapping map[string]string `json:"column_mapping"`
-	DateFormat    string            `json:"date_format"`
-}
-
-// Upload parses a CSV file and saves it to the database
-// The upload is expected to be a form with two fields:
+// Upload parses a CSV file and saves it to the pending transactions table
+// The upload is expected to be a form with the field:
 // - file: the CSV file
-// - meta: information about the CSV file, such as the column mapping
 func Upload(c echo.Context) error {
 
 	formFile, err := c.FormFile("file")
@@ -47,33 +39,22 @@ func Upload(c echo.Context) error {
 		return mustSendError(c, http.StatusInternalServerError, "error parsing file", err)
 	}
 
-	var meta meta
-	//marshal c.FormValue("meta") into meta struct
-	if err = json.Unmarshal([]byte(c.FormValue("meta")), &meta); err != nil {
-		return mustSendError(c, http.StatusBadRequest, "error parsing meta", err)
+	//"MM/DD/YYYY hh:MM:SS AM/PM"
+	transactionName := time.Now().Format("01-02-2006 15:04:05 PM")
+
+	transactions := models.PendingTransactions{
+		WIPTransactions:   parsedCSV,
+		SavedTransactions: nil,
+		Owner:             c.Get("username").(string),
+		Name:              transactionName,
 	}
 
-	renamedCSV, err := csv.RenameColumns(parsedCSV, meta.ColumnMapping)
-	if err != nil {
-		return mustSendError(c, http.StatusBadRequest, "error renaming columns", err)
-	}
-	err = csv.AdaptRequiredFields(renamedCSV, meta.DateFormat)
-	if err != nil {
-		return mustSendError(c, http.StatusBadRequest, "error adapting csv", err)
-	}
-
-	transactions := models.Transactions{
-		Transactions: renamedCSV,
-		Owner:        c.Get("username").(string),
-		Columns:      csv.GetColumns(renamedCSV),
-	}
-
-	err = dbs.DB.TransactionDao().Insert(&transactions)
+	err = dbs.DB.PendingTransactionsDao().Create(&transactions)
 	if err != nil {
 		return mustSendError(c, http.StatusInternalServerError, "error saving record", err)
 	}
 
-	BroadcastTransactions(c.Get("username").(string))
+	//BroadcastTransactions(c.Get("username").(string))
 
 	return nil
 }
