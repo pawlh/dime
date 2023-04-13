d61bce497c77f5ba0dbc1677047d842c4f887041
Paul Hathaway <paulchathaway@gmail.com>
2023-04-13 16:26:59 -0600

    add field for marshalling id

--- internal/models/pending_transactions.go
+++ internal/models/pending_transactions.go
@@ -2,8 +2,9 @@ package models
 
 // PendingTransactions holds transactions that have not yet been submitted by a user
 type PendingTransactions struct {
-	WIPTransactions   []map[string]any `bson:"wip_transactions" json:"wip_transactions"`
-	SavedTransactions []map[string]any `bson:"transactions" json:"transactions"`
-	Owner             string           `bson:"owner" json:"owner"`
-	Name              string           `bson:"name" json:"name"`
+	WIPTransactions    []map[string]any `bson:"wip_transactions" json:"wip_transactions"`
+	SavedTransactions  []map[string]any `bson:"transactions" json:"transactions"`
+	Owner              string           `bson:"owner" json:"owner"`
+	Name               string           `bson:"name" json:"name"`
+	TransactionGroupId int              `bson:"_id" json:"transaction_group_id"`
 }
