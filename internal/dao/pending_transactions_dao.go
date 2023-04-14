4edcb1053d746731d44d4b9e21aae03262607d4b
Paul Hathaway <paulchathaway@gmail.com>
2023-04-13 23:42:57 -0600

    add methods to interface

--- internal/dao/pending_transactions_dao.go
+++ internal/dao/pending_transactions_dao.go
@@ -7,4 +7,6 @@ import (
 type PendingTransactionsDao interface {
 	Create(transactions *models.PendingTransactions) error
 	FindByOwner(owner string) (*models.PendingTransactions, error)
+	FindById(id string) (*models.PendingTransactions, error)
+	Save(transactions *models.PendingTransactions) error
 }
