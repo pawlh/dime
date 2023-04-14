aeb38bc11c06be8e74f78a03fe3782c8d713077a
Paul Hathaway <paulchathaway@gmail.com>
2023-04-13 23:42:39 -0600

    add required field checks

--- internal/dao/mongodb/pending_transactions_dao.go
+++ internal/dao/mongodb/pending_transactions_dao.go
@@ -2,6 +2,7 @@ package mongodb
 
 import (
 	"dime/internal/models"
+	"errors"
 	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/mongo"
 )
@@ -16,6 +17,17 @@ func NewPendingTransactions(client *mongo.Client) PendingTransactions {
 
 // Create saves a new pending transaction to the database
 func (m PendingTransactions) Create(transactions *models.PendingTransactions) error {
+
+	if transactions.Owner == "" {
+		return errors.New("owner is required")
+	}
+	if transactions.Name == "" {
+		return errors.New("name is required")
+	}
+	if transactions.WIPTransactions == nil || len(transactions.WIPTransactions) == 0 {
+		return errors.New("wip_transactions is required")
+	}
+
 	collection := m.client.Database("dime").Collection("pending_transactions")
 
 	_, err := collection.InsertOne(nil, transactions)
@@ -36,4 +48,27 @@ func (m PendingTransactions) FindByOwner(owner string) (*models.PendingTransacti
 	}
 
 	return &transactions, nil
+}
+
+func (m PendingTransactions) FindById(id string) (*models.PendingTransactions, error) {
+	collection := m.client.Database("dime").Collection("transactions")
+
+	var transactions models.PendingTransactions
+	err := collection.FindOne(nil, bson.M{"_id": id}).Decode(&transactions)
+	if err != nil {
+		return nil, err
+	}
+
+	return &transactions, nil
+}
+
+func (m PendingTransactions) Save(transactions *models.PendingTransactions) error {
+	collection := m.client.Database("dime").Collection("transactions")
+
+	_, err := collection.ReplaceOne(nil, bson.M{"_id": transactions.TransactionGroupId}, transactions)
+	if err != nil {
+		return err
+	}
+
+	return nil
 }
