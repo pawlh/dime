9b30b97a3eaafe29daddca1f0648798db7927241
Paul Hathaway <paulchathaway@gmail.com>
2023-04-14 00:03:46 -0600

    add FindByOwner tests

--- internal/dao/mongodb/pending_transactions_dao_test.go
+++ internal/dao/mongodb/pending_transactions_dao_test.go
@@ -84,3 +84,59 @@ func TestPendingTransactions_Create(t *testing.T) {
 		})
 	}
 }
+
+func TestPendingTransactions_FindByOwner(t *testing.T) {
+	BeforeEach()
+	dao := NewPendingTransactions(client)
+
+	testTransactions := []map[string]any{
+		{
+			"amount":   100,
+			"date":     "2021-01-01",
+			"category": "Food",
+		},
+	}
+
+	err := dao.Create(&models.PendingTransactions{
+		WIPTransactions: testTransactions,
+		Name:            "test transaction group",
+		Owner:           "testUserA",
+	})
+	if err != nil {
+		t.Errorf("Error creating test pending transactions: %v", err)
+	}
+	err = dao.Create(&models.PendingTransactions{
+		WIPTransactions: testTransactions,
+		Name:            "test transaction group",
+		Owner:           "testUserB",
+	})
+	if err != nil {
+		t.Errorf("Error creating test pending transactions: %v", err)
+	}
+
+	tests := []struct {
+		name    string
+		owner   string
+		wantErr bool
+	}{
+		{
+			name:    "Find pending transactions by owner",
+			owner:   "testUserA",
+			wantErr: false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+
+			dao := NewPendingTransactions(client)
+
+			if transactions, err := dao.FindByOwner(tt.owner); (err != nil) != tt.wantErr {
+				t.Errorf("Create() error = %v, wantErr %v", err, tt.wantErr)
+			} else if !tt.wantErr {
+				if transactions.Owner != tt.owner {
+					t.Errorf("Owner does not match: %v", transactions.Owner)
+				}
+			}
+		})
+	}
+}
