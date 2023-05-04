9d722f8d30812ea4ba4179f98294b182c3cf2df2
Paul Hathaway <paulchathaway@gmail.com>
2023-05-03 22:37:04 -0600

    build out update request handling

--- internal/transaction/update.go
+++ internal/transaction/update.go
@@ -1,5 +1,12 @@
 package transaction
 
+import (
+	"dime/internal/dbs"
+	"errors"
+	"log"
+	"time"
+)
+
 type columnType string
 
 const (
@@ -15,45 +22,66 @@ const (
 	UndefinedUpdate    updateType = ""
 	ColumnNameUpdate              = "change_column_name"
 	RemoveColumnUpdate            = "remove_column"
-	AddColumnUpdate               = "add_column"
 	ColumnTypeUpdate              = "change_column_type"
 )
 
 type UpdateRequest struct {
-	TransactionGroupId int        `json:"transaction_group_id"`
+	TransactionGroupId string     `json:"transaction_group_id"`
 	UpdateType         updateType `json:"update_type"`
 	ColumnName         string     `json:"column_name"`
 	NewColumnName      string     `json:"new_column_name"`
 	NewColumnType      columnType `json:"new_column_type"`
+	DateFormat         string     `json:"date_format"`
 }
 
-func updatePendingTransaction(request UpdateRequest) {
-	//// Get the pending transaction
-	//pendingTransaction, err := dbs.DB.PendingTransactionsDao().FindByTransactionGroupId(request.TransactionGroupId)
-	//if err != nil {
-	//	log.Println("Error finding pending transaction:", err)
-	//	return
-	//}
-	//
-	//// Update the pending transaction
-	//switch request.UpdateType {
-	//case ColumnNameUpdate:
-	//	pendingTransaction.ColumnName = request.NewColumnName
-	//case RemoveColumnUpdate:
-	//	pendingTransaction.ColumnName = ""
-	//case AddColumnUpdate:
-	//	pendingTransaction.ColumnName = request.ColumnName
-	//	pendingTransaction.ColumnType = request.NewColumnType
-	//case ColumnTypeUpdate:
-	//	pendingTransaction.ColumnType = request.NewColumnType
-	//case UndefinedUpdate:
-	//	log.Println("Undefined update type")
-	//}
-	//
-	//// Save the pending transaction
-	//err = dbs.DB.PendingTransactionDao().SavePendingTransaction(pendingTransaction)
-	//if err != nil {
-	//	log.Println("Error saving pending transaction:", err)
-	//	return
-	//}
+func updatePendingTransaction(request UpdateRequest) error {
+	// Get the pending transaction
+	pendingTransaction, err := dbs.DB.PendingTransactionsDao().FindById(request.TransactionGroupId)
+	if err != nil {
+		log.Println("Error finding pending transaction:", err)
+		return err
+	}
+
+	// Update the pending transaction
+	switch request.UpdateType {
+	case ColumnNameUpdate:
+		for i := range pendingTransaction.WIPTransactions {
+			pendingTransaction.WIPTransactions[i][request.NewColumnName] = pendingTransaction.WIPTransactions[i][request.ColumnName]
+			delete(pendingTransaction.WIPTransactions[i], request.ColumnName)
+		}
+	case RemoveColumnUpdate:
+		for i := range pendingTransaction.WIPTransactions {
+			delete(pendingTransaction.WIPTransactions[i], request.ColumnName)
+		}
+	case ColumnTypeUpdate:
+		// this is a bit more complicated and prone to error
+		switch request.NewColumnType {
+		case DateType:
+			for i := range pendingTransaction.WIPTransactions {
+				date, err := time.Parse(request.DateFormat, pendingTransaction.WIPTransactions[i][request.ColumnName].(string))
+				if err != nil {
+					log.Println("Error parsing date:", err)
+					return errors.New("error parsing date: " + err.Error())
+				}
+
+				pendingTransaction.WIPTransactions[i][request.ColumnName] = date
+			}
+		case NumberType:
+			for i := range pendingTransaction.WIPTransactions {
+				// convert current value to number
+				pendingTransaction.WIPTransactions[i][request.ColumnName] = pendingTransaction.WIPTransactions[i][request.ColumnName].(float64)
+			}
+		}
+	case UndefinedUpdate:
+		return errors.New("undefined update type")
+	}
+
+	// Save the pending transaction
+	err = dbs.DB.PendingTransactionsDao().Save(pendingTransaction)
+	if err != nil {
+		log.Println("Error saving pending transaction:", err)
+		return err
+	}
+
+	return nil
 }
