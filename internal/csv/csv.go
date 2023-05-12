ed2add35339469592b0a41d2da3c10387456cb26
Paul Hathaway <paulchathaway@gmail.com>
2023-05-12 15:57:32 -0600

    remove csv operations and tests

--- internal/csv/csv.go
+++ internal/csv/csv.go
@@ -2,13 +2,10 @@ package csv
 
 import (
 	"bytes"
-	"dime/internal/models"
 	"encoding/csv"
-	"fmt"
-	"strconv"
-	"time"
 )
 
+// Parse parses a buffer (of a CSV file) into a slice of maps
 func Parse(buf *bytes.Buffer) ([]map[string]any, error) {
 
 	reader := csv.NewReader(buf)
@@ -36,64 +33,4 @@ func Parse(buf *bytes.Buffer) ([]map[string]any, error) {
 	}
 
 	return data, nil
-}
-
-func RenameColumns(data []map[string]any, columnMapping map[string]string) ([]map[string]any, error) {
-	var renamedData []map[string]any
-	for i, row := range data {
-		renamedData = append(renamedData, make(map[string]any))
-		for oldName, newName := range columnMapping {
-			if _, ok := row[oldName]; !ok {
-				return nil, fmt.Errorf("user-specified column %s is missing", oldName)
-			} else {
-				renamedData[i][newName] = row[oldName]
-			}
-		}
-	}
-
-	return renamedData, nil
-}
-
-func AdaptRequiredFields(data []map[string]any, dateFormat string) error {
-	for _, row := range data {
-		for columnName, columnType := range models.RequiredColumns {
-			if _, ok := row[columnName]; !ok {
-				return fmt.Errorf("column %s is missing", columnName)
-			}
-
-			switch columnType {
-			case "string":
-				//this should never be false
-				if _, ok := row[columnName].(string); !ok {
-					return fmt.Errorf("column %s is not a string", columnName)
-				}
-			case "date":
-				date, err := time.Parse(dateFormat, row[columnName].(string))
-				if err != nil {
-					return fmt.Errorf("could not format %s", row[columnName])
-				}
-				row[columnName] = date
-			case "float":
-				// convert string to float
-				number, err := strconv.ParseFloat(row[columnName].(string), 64)
-				if err != nil {
-					return fmt.Errorf("could not format %s", row[columnName])
-				}
-				row[columnName] = number
-			default:
-				return fmt.Errorf("unknown column type %s", columnType)
-			}
-		}
-	}
-
-	return nil
-}
-
-func GetColumns(data []map[string]any) []string {
-	var columns []string
-	for columnName := range data[0] {
-		columns = append(columns, columnName)
-	}
-
-	return columns
 }
