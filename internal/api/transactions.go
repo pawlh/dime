3a4d7999be0eaae006fa9a2d792d315d65117688
Paul Hathaway <paulchathaway@gmail.com>
2023-05-12 15:56:05 -0600

    remove websockets

--- internal/api/transactions.go
+++ internal/api/transactions.go
@@ -2,20 +2,8 @@ package api
 
 import (
 	"dime/internal/dbs"
-	"dime/internal/transaction"
-	"encoding/json"
-	"github.com/gorilla/websocket"
 	"github.com/labstack/echo/v4"
-	"log"
 	"net/http"
-)
-
-var (
-	upgrader = websocket.Upgrader{
-		CheckOrigin: func(r *http.Request) bool {
-			return true
-		},
-	}
 )
 
 func GetTransactions(c echo.Context) error {
@@ -27,35 +15,13 @@ func GetTransactions(c echo.Context) error {
 	return c.JSON(http.StatusOK, transactions.Transactions)
 }
 
+// GetPendingTransactions returns all pending transactions for the user
 func GetPendingTransactions(c echo.Context) error {
-	ws, err := upgrader.Upgrade(c.Response(), c.Request(), nil)
-	if err != nil {
-		log.Println(err)
-		return err
-	}
-
-	for {
-		_, message, err := ws.ReadMessage()
-		if err != nil {
-			log.Println(err)
-			break
-		}
-
-		var updateRequest transaction.UpdateRequest
-		err = json.Unmarshal(message, &updateRequest)
-		if err != nil {
-			err := ws.WriteMessage(websocket.TextMessage, []byte("Error unmarshalling data"))
-			if err != nil {
-				log.Println("Error sending error to client:", err)
-				return err
-			}
-		}
-
-		// Save the pending transactions
-		//err = dbs.DB.PendingTransactionsDao().SavePendingTransactions(data, c.Get("username").(string))
-
-	}
-
+	//transactions, err := dbs.DB.TransactionDao().FindPendingByOwner(c.Get("username").(string))
+	//if err != nil {
+	//	return mustSendError(c, http.StatusInternalServerError, "unable to search pending transactions", err)
+	//}
+	//
+	//return c.JSON(http.StatusOK, transactions.Transactions)
 	return nil
-
 }
