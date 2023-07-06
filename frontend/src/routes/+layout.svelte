26af61052c84db6baf721881a3c8cd1950527676
Paul Hathaway <paulchathaway@gmail.com>
2023-07-05 23:19:47 -0600

    import theme by ls preference

--- frontend/src/routes/+layout.svelte
+++ frontend/src/routes/+layout.svelte
@@ -1,5 +1,7 @@
 <script lang="ts">
+	import NavBar from '$lib/features/navbar/NavBar.svelte';
 	import { isLoggedIn, user } from '$lib/stores/auth';
+	import { theme } from '$lib/stores/theme';
 	import cookies from '$lib/utils/cookies';
 	import { navigateTo, getPage } from '$lib/utils/navigation';
 	import { onMount } from 'svelte';
@@ -18,4 +20,15 @@
 	});
 </script>
 
-<slot />
+<svelte:head>
+	<link rel="stylesheet" href={`/${$theme}Theme.css`} />
+</svelte:head>
+
+<div class="container">
+	<NavBar />
+	<slot />
+</div>
+
+<style lang="scss">
+	@import '/global.css';
+</style>
