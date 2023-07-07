2ffb3aa9793b648cabbc3193d4b23be9cff6644e
Paul Hathaway <paulchathaway@gmail.com>
2023-07-06 18:34:35 -0600

    use classes for theming

--- frontend/src/routes/+layout.svelte
+++ frontend/src/routes/+layout.svelte
@@ -1,34 +1,38 @@
 <script lang="ts">
-	import NavBar from '$lib/features/navbar/NavBar.svelte';
-	import { isLoggedIn, user } from '$lib/stores/auth';
-	import { theme } from '$lib/stores/theme';
-	import cookies from '$lib/utils/cookies';
-	import { navigateTo, getPage } from '$lib/utils/navigation';
-	import { onMount } from 'svelte';
+    import NavBar from '$lib/features/navbar/NavBar.svelte';
+    import {isLoggedIn, user} from '$lib/stores/auth';
+    import {theme} from '$lib/stores/theme';
+    import cookies from '$lib/utils/cookies';
+    import {getPage, navigateTo} from '$lib/utils/navigation';
+    import {onMount} from 'svelte';
 
-	$: if (!isLoggedIn) {
-		if (getPage().url.pathname !== '/auth/register') navigateTo('/auth/login');
-	}
 
-	// TODO: Remove this when there is a proper login flow
-	onMount(() => {
-		cookies.set('loggedIn', 'true', { path: '/' });
-		user.login({
-			firstName: 'Joe',
-			lastName: 'Tester'
-		});
-	});
+    $: if (!isLoggedIn) {
+        if (getPage().url.pathname !== '/auth/register') navigateTo('/auth/login');
+    }
+
+    // TODO: Remove this when there is a proper login flow
+    onMount(() => {
+        cookies.set('loggedIn', 'true', {path: '/'});
+        user.login({
+            firstName: 'Joe',
+            lastName: 'Tester'
+        });
+    });
 </script>
 
-<svelte:head>
-	<link rel="stylesheet" href={`/${$theme}Theme.css`} />
-</svelte:head>
-
-<div class="container">
-	<NavBar />
-	<slot />
+<div id="container" class={$theme === 'dark' ? 'dark-theme' : 'light-theme'}>
+    <NavBar/>
+    <h2>hi</h2>
+    <slot/>
 </div>
 
-<style lang="scss">
-	@import '/global.css';
+<style lang="scss" global>
+  @import './global.css';
+
+  #container {
+    background-color: var(--background--color);
+    color: var(--text--color--primary);
+  }
+
 </style>
