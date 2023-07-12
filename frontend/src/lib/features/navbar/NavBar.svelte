b39a415ee41f789a0e7e3abae8020b185b14e1ec
Paul Hathaway <paulchathaway@gmail.com>
2023-07-11 20:46:42 -0600

    create placeholder layouts

---
+++ frontend/src/lib/features/navbar/NavBar.svelte
@@ -0,0 +1,21 @@
+<script>
+	import UserInfo from './UserInfo.svelte';
+</script>
+
+<div class="container">
+    <UserInfo/>
+</div>
+
+<style lang="scss">
+  //.container {
+  //	display: flex;
+  //	flex-direction: column;
+  //}
+  //
+  //@media (min-width: 768px) {
+  //	.container {
+  //		width: 300px;
+  //		flex-direction: column;
+  //	}
+  //}
+</style>
---
+++ frontend/src/lib/features/navbar/UserInfo.svelte
@@ -0,0 +1,21 @@
+<script>
+	import { user } from '$lib/stores/auth';
+</script>
+
+<div class="container">
+	<!-- <img /> -->
+	<div>
+		<h3>{$user.firstName} {$user.lastName}</h3>
+	</div>
+</div>
+
+<style>
+	.container {
+		background-color: var(--surface--color--primary);
+
+		padding: 12px;
+
+		display: flex;
+		flex-direction: column;
+	}
+</style>
