ac76bda1cea69c497fcdaac54276225a20e13b84
Paul Hathaway <paulchathaway@gmail.com>
2023-07-21 23:43:21 -0600

    clear old work

--- cmd/dime/dime.go
+++ cmd/dime/dime.go
@@ -1,22 +1,4 @@
 package main
 
-import (
-	"dime/internal/dbs"
-	"dime/internal/server"
-	"log"
-	"os"
-)
-
 func main() {
-
-	dbHost := os.Getenv("MONGO_HOST")
-	if dbHost == "" {
-		log.Fatal("MONGO_HOST environment variable not set")
-	}
-	err := dbs.InitMongoDB("mongodb://" + dbHost)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	server.Start()
 }
--- internal/models/user.go
+++
@@ -1,8 +0,0 @@
-package models
-
-type User struct {
-	Id       int
-	Username string `json:"username,omitempty"`
-	Password string `json:"password,omitempty"`
-	Name     string `json:"name,omitempty"`
-}
