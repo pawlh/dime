cb8de5e61d2381f517f969f3815e0c81772739d2
Paul Hathaway <paulchathaway@gmail.com>
2023-07-25 18:55:17 -0600

    update docs

--- internal/database/database.go
+++ internal/database/database.go
@@ -3,9 +3,7 @@ package database
 import "dime/internal/database/dao"
 
 type Database interface {
-	// UserDAO returns a dao.UserDAO instance. Init() must be called before this method
 	UserDAO() (dao.UserDAO, error)
-	Init(string)
 
 	// Disconnect closes the connection to the database. This method should be called when no more database operations are needed
 	Disconnect()
