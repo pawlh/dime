aaf5cb24b254e68f4f3f02c69f98da92c6d812b1
Paul Hathaway <paulchathaway@gmail.com>
2023-07-26 19:41:55 -0600

    remove graphql

--- internal/api/routes.go
+++ internal/api/routes.go
@@ -1,19 +1,10 @@
 package api
 
 import (
-	"dime/graph"
-	"github.com/99designs/gqlgen/graphql/handler"
-	"github.com/99designs/gqlgen/graphql/playground"
 	"github.com/labstack/echo/v4"
 )
 
 func RegisterRoutes(e *echo.Echo) {
-
-	// GraphQL
-	srv := handler.NewDefaultServer(graph.NewExecutableSchema(graph.Config{Resolvers: &graph.Resolver{}}))
-	// TODO: only allow playground in dev mode
-	e.GET("/playground", echo.WrapHandler(playground.Handler("GraphQL playground", "/graphql")))
-	e.POST("/graphql", echo.WrapHandler(srv))
 
 	// Default
 	e.GET("/", func(c echo.Context) error {
