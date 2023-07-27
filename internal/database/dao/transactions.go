95eccec8bdc469b0e3e06ee92bc1869b78729912
Paul Hathaway <paulchathaway@gmail.com>
2023-07-26 19:06:58 -0600

    add docs

--- internal/database/dao/transactions.go
+++ internal/database/dao/transactions.go
@@ -5,9 +5,13 @@ import "dime/internal/models"
 type TransactionDAO interface {
 	// AddTransaction Add a transaction to the database
 	AddTransaction(transaction models.Transaction) (string, error)
+
 	// AddTransactions Add a list of transactions to the database.
 	AddTransactions(transactions []models.Transaction) error
+
 	// GetTransactions Fetch all transactions for a user
 	GetTransactions(owner string) ([]models.Transaction, error)
+
+	// Clear Removes all transactions from the database. This method should only be used for testing
 	Clear() error
 }
