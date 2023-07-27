1e7602dcd3a4c6c4f19a97078e83d9aa32f4d1a9
Paul Hathaway <paulchathaway@gmail.com>
2023-07-26 19:53:50 -0600

    add json tags

--- internal/models/transaction.go
+++ internal/models/transaction.go
@@ -3,17 +3,16 @@ package models
 import "time"
 
 type Transaction struct {
-	Id          string         `bson:"_id,omitempty"`
-	Owner       string         `bson:"owner"`
-	BaseFields  BaseFields     `bson:"baseFields"`
-	ExtraFields map[string]any `bson:"extraFields"`
+	Id          string         `bson:"_id,omitempty" json:"id"`
+	Owner       string         `bson:"owner" json:"owner"`
+	BaseFields  BaseFields     `bson:"baseFields" json:"baseFields"`
+	ExtraFields map[string]any `bson:"extraFields" json:"extraFields"`
 }
 
 type BaseFields struct {
-	Id          string    `bson:"id,omitempty"`
-	Date        time.Time `bson:"date"`
-	Description string    `bson:"description"`
-	Amount      float64   `bson:"amount"`
-	Category    string    `bson:"category"`
-	Account     string    `bson:"account"`
+	Date        time.Time `bson:"date" json:"date"`
+	Description string    `bson:"description" json:"description"`
+	Amount      float64   `bson:"amount" json:"amount"`
+	Category    string    `bson:"category" json:"category"`
+	Account     string    `bson:"account" json:"account"`
 }
