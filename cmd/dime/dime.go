a93a14ee6a4201202ae9ce2c0cb1d8824fe3f313
Paul Hathaway <paulchathaway@gmail.com>
2023-07-27 19:08:14 -0600

    verify that mongodb env var is present

--- cmd/dime/dime.go
+++ cmd/dime/dime.go
@@ -1,7 +1,14 @@
 package main
 
-import "dime/internal/server"
+import (
+	"dime/internal/server"
+	"os"
+)
 
 func main() {
+	if os.Getenv("MONGODB_URI") == "" {
+		panic("MONGODB_URI environment variable not set")
+	}
+
 	server.Start()
 }
