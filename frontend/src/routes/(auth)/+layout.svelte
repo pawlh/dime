ea557b569189d893f5717ddc5bbaf8e8fb036dc7
Paul Hathaway <paulchathaway@gmail.com>
2023-08-02 08:56:45 -0600

    add basic container for login/register

--- frontend/src/routes/(auth)/+layout.svelte
+++ frontend/src/routes/(auth)/+layout.svelte
@@ -7,4 +7,30 @@
     // });
 </script>
 
-<slot/>
+<div id="container">
+    <div class="auth-section">
+        <slot/>
+    </div>
+</div>
+
+<style>
+    #container {
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        justify-content: center;
+        height: 100vh;
+    }
+
+    .auth-section {
+        width: 400px;
+        height: 500px;
+        background-color: var(--surface--color--primary);
+        border-radius: 10px;
+        box-shadow: 0 0 10px 0 rgba(0, 0, 0, 0.2);
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        justify-content: center;
+    }
+</style>
